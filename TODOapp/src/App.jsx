2d105c2d42403f48d2476269398ef3ff42f9e229
aashish <hiaashish83@gmail.com>
2025-03-31 00:27:05 +0530

    added todoapp

---
+++ TODOapp/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+function App() {
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    setTodos([...todos, todo]);
+  }
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <input
+          onChange={(e) => setTodo(e.target.value)}
+          value={todo}
+          type="text"
+        />
+        <button type="submit">Add</button>
+      </form>
+      {console.log(todos)}
+    </>
+  );
+}
+
+export default App;
